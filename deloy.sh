bf1f8ef03433e4a5b36d69bea1f8231f49c5535d
Iris Huang <lyaui@outlook.com>
2020-04-24 00:49:25 +0800

    FIX

--- deloy.sh
+++ deloy.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env sh
-
 set -e
 
 npm run build
 
 cd dist
+
 git init
 git add -A
 git commit -m 'deploy'
 
+cd -
+
 git push -f git@github.com:lyaui/Hotel-booking.git master:gh-pages
 
-cd
+cd -
